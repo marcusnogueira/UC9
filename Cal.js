f7fc247e9dcc03c3465aa7980e40c3b217a1e1b2
Marcus R Nogueira <mn24243@srcs.loc>
2017-01-27 08:07:43 -0800

    new project

---
+++ Cal.js
@@ -0,0 +1,12 @@
+function changeSign() {
+    currentInput = currentInput * -1;
+    displayCurrentInput();
+}
+function percentage() {
+    currentInput = currentInput / 100;
+    displayCurrentInput();
+}
+function inverse() {
+    currentInput = 1 / currentInput;
+    displayCurrentInput();
+}
